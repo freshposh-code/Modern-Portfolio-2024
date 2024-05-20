bf691164529c05e7b2f297015266027b9d4d9de9
freshposh-code <atomisefarouk919@gmail.com>
2024-05-20 13:30:17 +0100

    commit hero set-up

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
-import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
 
 const Hero = () => {
     return (
@@ -29,8 +29,7 @@ const Hero = () => {
        *  0.2 to 0.03
        */}
             <div
-                className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
+                className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center"
             >
                 {/* Radial gradient for the container to give a faded look */}
                 <div
@@ -57,7 +56,7 @@ const Hero = () => {
                     />
 
                     <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                        Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+                        Hi! I&apos;m Farouk, a React.js Developer based in Nigeria.
                     </p>
 
                     <a href="#about">
